c2af58a75134f9e9d1f684e2e80e66524fed89be
Aadi <aadipapsarponmisti@gmail.com>
2020-08-30 14:12:22 +0530

    Create TABLET.cpp

---
+++ codechef/TABLET.cpp
@@ -0,0 +1,63 @@
+/*
+                         _______________________________
+    /"""""""""""\      ,'                               `.
+ _ / ____   ____ \ _  (        WELCOME TO MY CODE         )
+| \=( (@ )=( (@ )=/ |  `.  _____________________________,'
+\_( ,`--'(_)`--'. )_/    |/
+ ( /______I______\ )   _,'
+  \\\_|_|_|_|_|_///
+   \ `|_|_|_|_|' /
+    `---.___,---'
+       `=---='
+ 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  Amirul Islam IIITBH CSE 2018-2022 || Code-chef || Loading.... 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+
+#define ll                    long long int
+#define loop(i,a,b)           for(int i=(int)a;i<(int)b;++i)
+#define rloop(i,a,b)          for(int i=(int)a;i<=(int)b;++i)
+#define loopl(i,a,b)          for(ll i=(ll)a;i<(ll)b;++i)
+#define loopr(i,a,b)          for(int i=(int)a;i>=(int)b;--i)
+#define ab(a)                 (a<0)?(-1*a):a
+#define MOD                   1000000007
+#define MAX                   1e9
+#define MIN                   -1e9
+#define max(x,y)              (x>y)?x:y
+#define min(x,y)              (x<y)?x:y
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  int test; scanf("%d",&test); while(test--)
+using namespace std;
+
+int main()
+{
+	sv(){
+		int N,B;
+		cin>>N>>B;
+		int ans = -1;
+		while(N--){
+			int a,b,c; cin>>a>>b>>c;
+			if(c <= B)
+				ans = max(ans, a*b);
+		}
+		if(ans == -1)
+			cout<<"no tablet"<<endl;
+		else 
+			cout<<ans<<endl;
+	}
+	return 0;
+}
+
