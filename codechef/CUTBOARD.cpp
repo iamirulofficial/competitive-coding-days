c3e425d74be9a46d09cf1acc4ec171928c4c14aa
Aadi <aadipapsarponmisti@gmail.com>
2020-08-30 14:16:27 +0530

    Create CUTBOARD.cpp

---
+++ codechef/CUTBOARD.cpp
@@ -0,0 +1,54 @@
+/*
+                         _______________________________
+    /"""""""""""\      ,'                               `.
+ _ / ____   ____ \ _  (        WELCOME TO MY CODE         )
+| \=( (@ )=( (@ )=/ |  `.  _____________________________,'
+\_( ,`--'(_)`--'. )_/    |/
+ ( /______I______\ )   _,'
+  \\\_|_|_|_|_|_///
+   \ `|_|_|_|_|' /
+    `---.___,---'
+       `=---='
+ 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  Amirul Islam IIITBH CSE 2018-2022 || Code-chef || Loading.... 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+
+#define ll                    long long int
+#define loop(i,a,b)           for(int i=(int)a;i<(int)b;++i)
+#define rloop(i,a,b)          for(int i=(int)a;i<=(int)b;++i)
+#define loopl(i,a,b)          for(ll i=(ll)a;i<(ll)b;++i)
+#define loopr(i,a,b)          for(int i=(int)a;i>=(int)b;--i)
+#define ab(a)                 (a<0)?(-1*a):a
+#define MOD                   1000000007
+#define MAX                   1e9
+#define MIN                   -1e9
+#define max(x,y)              (x>y)?x:y
+#define min(x,y)              (x<y)?x:y
+#define mid(s,e)              (s+(e-s)/2)
+#define sv()                  int test; scanf("%d",&test); while(test--)
+using namespace std;
+
+int main()
+{
+	sv(){
+	    int m,n;
+	    cin>>m>>n;
+	    cout<<(m-1)*(n-1)<<endl;
+	}
+	return 0;
+}
+
